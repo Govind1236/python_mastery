4630484c5223bc042bfb9d2f2341f5b70bc5401c
Govind Basnet <govindbasnet02@gmail.com>
2025-06-05 22:44:10 +0545

    Initialize

---
+++ Day_1_Projects/day_2_data_types.py
@@ -0,0 +1,10 @@
+# my_age = 25
+# print("My age is", my_age)
+# price = 3.14
+# print("The price is", price)
+tshirt =  5
+total_tshirt = tshirt + 5
+print("The total tshirt is", total_tshirt)
+price_of_tshirt = 175.5
+total_cost = price_of_tshirt * total_tshirt
+print("The Total Tshirt Price is", total_cost)
---
+++ Day_1_Projects/number_types_practice.py
@@ -0,0 +1,11 @@
+# int = 4
+# integer = int + 2
+# print(type(integer))
+num_items_purchase = 7
+cost_per_item = 2.5
+total_cost = num_items_purchase * cost_per_item
+print(f"The Total Cost For The Item Purchase is {total_cost}")
+
+total_pepole = 15
+bus_limit = 4
+print(f"The total limit is {total_pepole // bus_limit}")
