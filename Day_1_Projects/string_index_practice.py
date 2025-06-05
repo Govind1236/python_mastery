e3373858183f7489a94470f3b376d2bb15ee0e3b
Govind Basnet <govindbasnet02@gmail.com>
2025-06-05 23:12:36 +0545

    feat:add slicing

---
+++ Day_1_Projects/string_index_practice.py
@@ -0,0 +1,12 @@
+# animal = "ELEPHANT"
+# print(f"The First Letter of animal is: {animal[0]}")
+# print(f"The P Letter of animal is: {animal[3]}")
+# print(f"The Last Letter of animal is: {animal[7]}")
+# print(f"The L Letter of animal is: {animal[1]}")
+
+# String Manupulaion
+phrase = "Python is Powerful and Fun"
+word_is = phrase[7:9]
+print(f"The word is: {word_is}")
+word_powerful = phrase[10:18]
+print(f"The word is: {word_powerful}")
