e07a4b76c3401d1a9e56b1d6ec88f75ad324f3e8
Govind Basnet <govindbasnet02@gmail.com>
2025-06-05 23:25:19 +0545

    feat: string immutable

--- Day_1_Projects/string_index_practice.py
+++ Day_1_Projects/string_index_practice.py
@@ -5,8 +5,16 @@
 # print(f"The L Letter of animal is: {animal[1]}")
 
 # String Manupulaion
-phrase = "Python is Powerful and Fun"
-word_is = phrase[7:9]
-print(f"The word is: {word_is}")
-word_powerful = phrase[10:18]
-print(f"The word is: {word_powerful}")
+# phrase = "Python is Powerful and Fun"
+# word_is = phrase[7:9]
+# print(f"The word is: {word_is}")
+# word_powerful = phrase[10:18]
+# print(f"The word is: {word_powerful}")
+
+# immutable String
+secret_message = "heLLO WORld"
+print(f"Original String: {secret_message}")
+firs_char_capitialize = secret_message[0].upper()
+rest_of_message_lowercase = secret_message[1:].lower()
+clear_message = firs_char_capitialize + rest_of_message_lowercase
+print(f"Clear Message is: {clear_message}")
