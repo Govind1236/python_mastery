ac164306a43a19af5fad6f7a88dc7199f52e7a63
Govind Basnet <govindbasnet02@gmail.com>
2025-09-05 12:57:07 +0545

    conditions

---
+++ Day_1_Projects/Introduction/ifelse.py
@@ -0,0 +1,13 @@
+# check = int(input("Enter a number: "))
+# if check % 2 == 0 :
+#     print("Its Even")
+# else:
+#     print("Its Odd")
+
+value = int(input("Enter a Number: "))
+if value > 0:
+    print("Positive")
+elif value < 0:
+    print("Negative")
+else:
+    print("Zero")
