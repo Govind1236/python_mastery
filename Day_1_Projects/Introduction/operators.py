64057469fa8883cdda63a73750af5e96662d1ef2
Govind Basnet <govindbasnet02@gmail.com>
2025-09-05 10:51:30 +0545

    operator

---
+++ Day_1_Projects/Introduction/operators.py
@@ -0,0 +1,39 @@
+# Write a program to input two numbers and print their sum, difference, product, and quotient.
+
+# Find the remainder when 55 is divided by 6.
+
+# Calculate the square and cube of a number using **.
+
+"""A = int(input("Enter First Number: "))
+B = int(input("Enter Second Number: "))
+add = A + B
+sub = A - B
+mul = A * B
+Mod = A % B
+square = A**2
+print (f" Add: {add}\n Sub: {sub}\n Mul: {mul}\n Mod: {Mod}\n Square: {square}")
+print( 8 >> 2)"""
+
+# Take two inputs and check which one is greater.
+
+"""a = 3
+b = 5
+print (a < b)
+print (a > b)"""
+# Check if a given number is equal to 100.
+"""a = 100
+b = 50
+print(a == b)
+print(a != b)"""
+# Write a program to compare age of two people and print who is older or if they are the same age.
+
+"""a_age = 34
+b_age = 34
+if a_age > b_age:
+    print(" Person is older than B", a_age)
+elif a_age == b_age:
+    print( " Person are of same Age", )
+else:
+    print(" B is older")
+"""
+
