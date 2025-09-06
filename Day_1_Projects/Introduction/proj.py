bbba8db15a43934c00e400fe11a3fd76d20b4187
Govind Basnet <govindbasnet02@gmail.com>
2025-09-06 18:42:19 +0545

    switch choice

---
+++ Day_1_Projects/Introduction/proj.py
@@ -0,0 +1,17 @@
+try:
+    num = float(input("Enter a First Number: "))
+    num1 = float(input("Enter a Second Number: "))
+except ValueError:
+    print("Enter a correct numbers")
+else:
+    choice = input("Enter operators to do the task * + / - .... : ")
+    if choice == '+':
+        print(f"Addition: {num + num1}")
+    elif choice == '-':
+     print(f"Subtraction: {num - num1}")
+    elif choice == '/':
+        print(f"Division: {num / num1}")
+    elif choice == '%':
+        print(f"Moduls: {num % num1}")
+    elif choice == '*':
+        print(f"Multiplication: {num * num1}")
