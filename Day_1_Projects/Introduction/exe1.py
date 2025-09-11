145594f4f6add815bbf79034906bf416856cc0f7
Govind Basnet <govindbasnet02@gmail.com>
2025-09-11 18:11:22 +0545

    mail checker

---
+++ Day_1_Projects/Introduction/exe1.py
@@ -0,0 +1,17 @@
+# Given words = ["I","love","Python"], make: "I love Python".
+
+# words = ["I","love","Python"]
+# print(words)
+# result = " ".join(words)
+# print(f"The Result is: {result}")
+# words = "I,love,Python"
+# print(words)
+# result = words.split(",")
+# print(f"The Result is: {result}")
+
+email = input("Enter a Email: ")
+if "@" and "gmail" in email:
+    print("Valid Email")
+else:
+    print("Invalid Email")
+print(email)
